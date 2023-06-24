2b8fcde5c7d32280cb13051329f325284c715c17
wayne <136881856+bahtitoshkenskiy@users.noreply.github.com>
2023-06-24 11:03:56 +0500

    ывп

--- src/pages/Konor.js
+++ src/pages/Konor.js
@@ -1,21 +1,109 @@
 import React, { Component } from 'react'
 import './Konor.css';
+import { AiOutlineStar,AiFillStar,AiOutlineArrowRight } from "react-icons/ai";
 
 export default class Konor extends Component {
   render() {
     return (
       <div>
+{/* <div className="blue">
+<div className="text">
+  <h1 className='h1' >Biz sizga Dasturla
+va boshqa kurslarni taklif 
+qilamiz.</h1>
+<h3 className='h2'>Siz istalgan vaqtda har qanday mavzuda kurslarni o’rganishingiz 
+mumkin. Buning uchun qidiruv tizimidan foydalaning</h3>
+<input className='inp1' type="text" placeholder='Qanday kurs o’rganmoqchisiz ?'/>
+</div>
+</div>
+<div className="yelow"></div> */}
+
+
+
+
+        <div className="bg">
         <div className="big">
             <div className="text">
             <h1 className='h1'>Biz sizga Dasturla
 va boshqa kurslarni taklif 
 qilamiz.</h1>
 <p className='p1'>Siz istalgan vaqtda har qanday mavzuda kurslarni o’rganishingiz 
-mumkin. Buning uchun qidiruv tizimidan foydalaning</p>
+</p>
 <input placeholder='Qanday kurs o’rganmoqchisiz ?' className='inp1' type="text" />
 </div>
         </div>
         <div className="yelow"></div>
+        </div>
+        <div className="boss">
+          <div className="mini2">
+<div className="uzs1">
+      <div className="mini4">
+        <button className='bt4'>Marketing</button>
+        <h3>250.000 uzs</h3>
+      </div>
+<div className="mini6">
+  <div className="mini5">
+  <h3 className='h3'>Raqamli marketing bo'yicha 
+to'liq kurs</h3>
+<div className="stars">
+  <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/><AiOutlineStar/>4.1(524)
+</div>
+  </div>
+  <button className='bt5'><AiOutlineArrowRight/></button>
+</div>
+</div>
+
+<div className="uzs2">
+      <div className="mini4">
+        <button className='bt6'>Dasturlash</button>
+        <h3>250.000 uzs</h3>
+      </div>
+<div className="mini6">
+  <div className="mini5">
+  <h3 className='h3'>Java Spring bo’yicha to’liq
+kurs(amaliy)</h3>
+<div className="stars">
+  <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/><AiOutlineStar/>4.0(670)
+</div>
+  </div>
+  <button className='bt5'><AiOutlineArrowRight/></button>
+</div>
+</div>
+<div className="uzs3">
+      <div className="mini4">
+        <button className='bt7'>Dizayn</button>
+        <h3>400.000 uzs</h3>
+      </div>
+<div className="mini6">
+  <div className="mini5">
+  <h3 className='h3'>Illustrator 0 dan professionalgacha
+to’liq kurs</h3>
+<div className="stars">
+  <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/><AiOutlineStar/>4.1(1,250)
+</div>
+  </div>
+  <button className='bt5'><AiOutlineArrowRight/></button>
+</div>
+</div>
+<div className="uzs4">
+      <div className="mini4">
+        <button className='bt7'>Dizayn</button>
+        <h3>400.000 uzs</h3>
+      </div>
+<div className="mini6">
+  <div className="mini5">
+  <h3 className='h3'>Illustrator 0 dan professionalgacha
+to’liq kurs</h3>
+<div className="stars">
+  <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/><AiOutlineStar/>4.1(1,250)
+</div>
+  </div>
+  <button className='bt5'><AiOutlineArrowRight/></button>
+</div>
+</div>
+          </div>
+          <div className="mini3"></div>
+         </div> 
       </div>
     )
   }
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { RiArrowDownSLine} from "react-icons/ri";
 
 export default class Start extends Component {
