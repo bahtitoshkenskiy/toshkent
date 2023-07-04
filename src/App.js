3f44799052c0f39f45ea3a8769dd13d7bb9562dc
wayne <136881856+bahtitoshkenskiy@users.noreply.github.com>
2023-07-04 11:00:53 +0500

    fruy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Start from './pages/Start.js'
 import Konor from './pages/Konor.js'
+import { RiSearch2Line } from "react-icons/ri";
 
 function App() {
   return (
@@ -12,17 +13,18 @@ function App() {
   <h1>Biz sizga <span> Dasturla</span>
 va boshqa kurslarni taklif 
 qilamiz.</h1>
-  <p></p>
+  <h5>Siz istalgan vaqtda har qanday mavzuda kurslarni o’rganishingiz 
+mumkin. Buning uchun qidiruv tizimidan foydalaning</h5>
   <div className="inp">
-    <input type="text" />
+    <input type="text" placeholder='Qanday kurs o’rganmoqchisiz ?'/><RiSearch2Line/>
   </div>
 </div>
 <div className="aylana"></div>
 <div className="cards">
+  <div id='card01' className="card1"></div>
   <div className="card1"></div>
-  <div className="card1"></div>
-  <div className="card1"></div>
-  <div className="card1"></div>
+  <div  className="card1"></div>
+  <div id='card1' className="card1"></div>
 </div>
 </header>
     </div>
